ea7780fb076a59f48f209e935d7347a212aff1a4
Zachary B Hatch <zh86549@srcs.loc>
2018-05-23 11:40:59 -0700

    fine

---
+++ animate.js
@@ -0,0 +1,52 @@
+var box = document.getElementById("yeet");
+var xpos = 0;
+var ypos = 0;
+var xstep = 4;
+var ystep = 3;
+var id = setInterval(frame, 4);
+
+function frame() {
+    if (xpos > 1380 || xpos < 0) {
+        xstep = -xstep;
+    }
+     if (ypos > 500 || ypos < 0) {
+        ystep = -ystep;
+    }
+
+    xpos = xpos + xstep;
+    ypos = ypos + ystep;
+    box.style.top = ypos + 'px';
+    box.style.left = xpos + 'px';
+}
+var thing = document.getElementById("thing");
+var container = document.getElementById("container");
+var mousecoords = document.getElementById("mousecoords");
+
+container.addEventListener("mousemove", printMouseCoords);
+
+function printMouseCoords(event) {
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+    mousecoords.textContent = mouseX + ", " + mouseY;
+}
+
+container.addEventListener("click", moveThing);
+
+function moveThing(event) {
+    var xPosition = event.clientX - container.getBoundingClientRect().left - (thing.clientWidth / 2);
+	var yPosition = event.clientY - container.getBoundingClientRect().top - (thing.clientHeight / 2);
+	// in case of a wide border, the boarder-width needs to be considered in the formula above
+	thing.style.left = xPosition + "px";
+	thing.style.top = yPosition + "px";
+}
+window.addEventListener("keypress", changeThingColor);
+
+function changeThingColor(event) {
+    if (event.key === "b"){
+        thing.style.background = "blue";
+    } else if (event.key === "o") {
+        thing.style.background = "orange";
+    } else {
+        thing.style.background = "green";
+    }
+}
